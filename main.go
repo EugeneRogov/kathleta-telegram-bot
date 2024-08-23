6f0666c6b5b32185ac0a94dcf95c0abbe0b177bc
Eugene Rogov <digest2003@mail.ru>
2024-08-23 16:53:11 +0300

    init

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+	t := mustToken()
+	// fetcher = telegram.New(token)
+
+	// fmt.Println("Привет, Мир!") // ВЫВОД: Привет, Мир!
+}
+
+func mustToken() string {
+	token := flag.String(
+		"token-bot-token",
+		"",
+		"token for access to telegram bot",
+	)
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
+}
